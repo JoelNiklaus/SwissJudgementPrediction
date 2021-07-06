d6447d86a47558471fbcd700662fb5697b4283bb
Joel Niklaus <me@joelniklaus.ch>
2021-07-06 16:53:34 +0200

    added some more automation to run script

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ MODEL_NAME="xlm-roberta-base"
 # English models
 #MODEL_NAME="bert-base-cased"
 #MODEL_NAME="bert-large-cased"
-#MODEL_NAME="allenai/longformer-base-4096"      #needs debugging
+#MODEL_NAME="allenai/longformer-base-4096"      # needs debugging
 #MODEL_NAME="allenai/longformer-large-4096"
 #MODEL_NAME="google/bigbird-roberta-base"
 #MODEL_NAME="google/bigbird-roberta-large"
@@ -33,9 +33,11 @@ MODEL_NAME="xlm-roberta-base"
 # Batch size for RTX 3090 for
 # Distilbert: 64
 # BERT-base: 16
-# BERT-large: 4?, 8?
-# HierBERT (input size 4096) Distilbert: 4?
-# HierBERT (input size 4096) BERT-base: 2
+# BERT-large: 8
+# HierBERT/longformer (input size 4096) Distilbert: 8?
+# HierBERT/longformer (input size 4096) BERT-base: 2
+# HierBERT/longformer (input size 2048) BERT-base: 4
+# HierBERT/longformer (input size 1024) BERT-base: 8
 # LongBERT (input size 2048) BERT-base: 2
 # LongBERT (input size 1024) BERT-base: 4
 
@@ -48,13 +50,12 @@ MAX_SAMPLES=100
 
 # IMPORTANT: For bigger models, very small total batch sizes did not work (4 to 8), for some even 32 was too small
 BASE_DIR='sjp'
-TYPE='standard'    # one of 'standard', 'long', 'hierarchical'
-MAX_SEQ_LENGTH=512 # how many tokens to consider as input (hierarchical/long: 2048 is enough for facts)
-LR=3e-5            # Devlin et al. suggest somewhere in {1e-5, 2e-5, 3e-5, 4e-5, 5e-5}
-BATCH_SIZE=16      # depends on how much we can fit on the gpu
+TYPE='hierarchical' # one of 'standard', 'long', 'longformer', 'hierarchical'
+LR=3e-5             # Devlin et al. suggest somewhere in {1e-5, 2e-5, 3e-5, 4e-5, 5e-5}
+BATCH_SIZE=4        # depends on how much we can fit on the gpu
 TOTAL_BATCH_SIZE=64
 NUM_EPOCHS=5
-LANG='fr'
+LANG='de'
 SEED=$1
 
 # Compute variables based on settings above
@@ -62,8 +63,7 @@ MODEL=$MODEL_NAME-$TYPE
 DIR=$BASE_DIR/$MODEL/$LANG/$SEED
 ACCUMULATION_STEPS=$(($TOTAL_BATCH_SIZE / $BATCH_SIZE)) # use this to achieve a sufficiently high total batch size
 # Assign variables for enabling/disabling respective BERT version
-[ "$TYPE" == "long" ] && LONG_BERT="True" || LONG_BERT="False"
-[ "$TYPE" == "hierarchical" ] && HIER_BERT="True" || HIER_BERT="False"
+[ "$TYPE" == "standard" ] && MAX_SEQ_LENGTH=512 || MAX_SEQ_LENGTH=2048 # how many tokens to consider as input (hierarchical/long: 2048 is enough for facts)
 
 MODE='train'                                            # Can be either 'train' or 'evaluate'
 [ "$MODE" == "train" ] && TRAIN="True" || TRAIN="FALSE" # disable training if we are not in train mode
@@ -75,10 +75,9 @@ CHECKPOINT=""
 python run_tc.py \
   --problem_type "single_label_classification" \
   --model_name_or_path $MODEL_PATH \
-  --run_name $MODEL-$SEED \
+  --run_name $MODEL-$LANG-$SEED \
   --output_dir $DIR \
-  --use_long_bert $LONG_BERT \
-  --use_hierarchical_bert $HIER_BERT \
+  --long_input_bert_type $TYPE \
   --learning_rate $LR \
   --seed $SEED \
   --language $LANG \
@@ -102,7 +101,7 @@ python run_tc.py \
   --metric_for_best_model eval_loss \
   --save_total_limit 10 \
   --report_to $REPORT \
-  --overwrite_output_dir True \
+  --overwrite_output_dir False \
   --overwrite_cache False \
   $MAX_SAMPLES_ENABLED
 
