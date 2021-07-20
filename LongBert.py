860cbcb2ac6f4f8e4f35de553ecfe462a47ed26f
Joel Niklaus <me@joelniklaus.ch>
2021-07-20 13:54:07 +0200

    fixed longbert problem with reloading

--- LongBert.py
+++ LongBert.py
@@ -16,17 +16,17 @@ def resize_position_embeddings(encoder, max_length, device):
     new_embeddings = torch.nn.Embedding(embedding_length, encoder.config.hidden_size).to(device)
 
     # Replicate embeddings N times
-    for N in range(int(max_length / old_max_length)):
+    for N in range(int(embedding_length / old_max_length)):
         new_embeddings.weight.data[N * old_max_length:(N + 1) * old_max_length, :] = \
             encoder.embeddings.position_embeddings.weight.data[:old_max_length, :]
     if encoder.config.model_type in ['camembert', 'xlm-roberta']:
         # Fill in the last two weights because the embedding_length is higher for RoBERTa
         new_embeddings.weight.data[-2:] = encoder.embeddings.position_embeddings.weight.data[-2:, :]
     encoder.embeddings.position_embeddings = new_embeddings
-    encoder.embeddings.position_ids = torch.arange(max_length).expand((1, -1)).to(device)
-    encoder.embeddings.token_type_ids = torch.zeros(max_length, dtype=torch.int).expand((1, -1)).to(device)
+    encoder.embeddings.position_ids = torch.arange(embedding_length).expand((1, -1)).to(device)
+    encoder.embeddings.token_type_ids = torch.zeros(embedding_length, dtype=torch.int).expand((1, -1)).to(device)
     # Fix config values
-    encoder.config.max_position_embeddings = max_length
-    encoder.max_position_embeddings = max_length
+    encoder.config.max_position_embeddings = embedding_length
+    encoder.max_position_embeddings = embedding_length
 
     return encoder
